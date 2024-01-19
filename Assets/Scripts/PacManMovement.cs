bf0d0d63a138cc8d86b0abde12f07907e2e9a13d
efraitor_ <efraitor.contact@gmail.com>
2024-01-19 13:57:05 +0100

    Update
    Added PacMan animation and movement

--- Assets/Scripts/PacManMovement.cs
+++ Assets/Scripts/PacManMovement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PacManMovement : MonoBehaviour
 {
     //Velocidad de PacMan
-    public float speed = 0.4f;
+    public float speed = 2f;
     //Referencia al RigidBody
     public Rigidbody2D theRB;
+    //Referencia al Animator
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +23,34 @@ public class PacManMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             theRB.velocity = new Vector2(-speed, 0f);
+            //Cambiamos el parámetro del Animator (DirX) para que haga animación izquierda
+            anim.SetFloat("DirX", -1);
+            //Anulamos la DirY
+            anim.SetFloat("DirY", 0);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             theRB.velocity = new Vector2(speed, 0f);
+            //Cambiamos el parámetro del Animator (DirX) para que haga animación derecha
+            anim.SetFloat("DirX", 1);
+            //Anulamos la DirY
+            anim.SetFloat("DirY", 0);
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             theRB.velocity = new Vector2(0f, speed);
+            //Cambiamos el parámetro del Animator (DirY) para que haga animación arriba
+            anim.SetFloat("DirY", 1);
+            //Anulamos la DirX
+            anim.SetFloat("DirX", 0);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             theRB.velocity = new Vector2(0f, -speed);
+            //Cambiamos el parámetro del Animator (DirY) para que haga animación abajo
+            anim.SetFloat("DirY", -1);
+            //Anulamos la DirX
+            anim.SetFloat("DirX", 0);
         }
     }
 
