cf21a3fb1ac1e84bf74024ee70e0902d6b06fd93
efraitor_ <efraitor.contact@gmail.com>
2024-01-23 13:10:50 +0100

    Update
    Added Ghosts

---
+++ Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    //Necesitamos un array de posiciones llamado Waypoints(puntos de ruta, las chinchetas).
+    //Cada fantasma puede tener un número de puntos de ruta distinto
+    public Transform[] waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
